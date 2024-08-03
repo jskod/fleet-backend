572f490a0bfe916e7db710996679a735008ab15f
Tauqeer Nasir <tauqeer.insta@gmail.com>
2024-08-03 15:36:20 +0400

    improve test coverage

--- src/modules/queues/consumers/create-tracking-consumer.spec.ts
+++ src/modules/queues/consumers/create-tracking-consumer.spec.ts
@@ -63,4 +63,80 @@ describe('CreateTrackingConsumer', () => {
 
     expect(mockTrackingService.insert).toHaveBeenCalledWith(job.data);
   });
+
+  it('should log job processing when activating the job', () => {
+    const job: Job<CreateTrackingQueueDataType> = {
+      id: 'test-1',
+      data: {
+        vehicleId: 'test1',
+        timestamp: '2024-08-03T23:58:16.038Z',
+        location: {
+          latitude: 37.793169190714124,
+          longitude: -122.37991487676067,
+          altitude: 28.99365501009916,
+          heading: 218.45610950238648,
+          speed: 60,
+        },
+        vehicleStatus: {
+          engineRunning: false,
+          fuelLevel: 15.35,
+          batteryVoltage: 12.14,
+          odometer: 2050,
+          tirePressure: {
+            frontLeft: 30.19,
+            frontRight: 32.05,
+            rearLeft: 30.83,
+            rearRight: 33.6,
+          },
+        },
+        alerts: [],
+      },
+    } as any;
+
+    const spyConsole = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+    provider.onActive(job);
+
+    expect(spyConsole).toHaveBeenCalledWith(
+      expect.stringContaining('job test-1'),
+    );
+  });
+
+  it('should log error when job is failed', () => {
+    const job: Job<CreateTrackingQueueDataType> = {
+      id: 'test-1',
+      data: {
+        vehicleId: 'test1',
+        timestamp: '2024-08-03T23:58:16.038Z',
+        location: {
+          latitude: 37.793169190714124,
+          longitude: -122.37991487676067,
+          altitude: 28.99365501009916,
+          heading: 218.45610950238648,
+          speed: 60,
+        },
+        vehicleStatus: {
+          engineRunning: false,
+          fuelLevel: 15.35,
+          batteryVoltage: 12.14,
+          odometer: 2050,
+          tirePressure: {
+            frontLeft: 30.19,
+            frontRight: 32.05,
+            rearLeft: 30.83,
+            rearRight: 33.6,
+          },
+        },
+        alerts: [],
+      },
+    } as any;
+
+    const spyConsole = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+    provider.onError(job);
+
+    expect(spyConsole).toHaveBeenCalledWith(
+      expect.stringContaining('Error processing job test-1'),
+    );
+  });
 });
--- src/modules/tracking/tracking.service.spec.ts
+++ src/modules/tracking/tracking.service.spec.ts
@@ -32,6 +32,7 @@ describe('TrackingService', () => {
             create: jest.fn(),
             save: jest.fn(),
             find: jest.fn(),
+            aggregate: jest.fn(),
           },
         },
       ],
@@ -180,5 +181,82 @@ describe('TrackingService', () => {
     expect(mockTrackingQueue.add).toHaveBeenCalledWith('add-tracking', data);
     expect(result.name).toBe('add-tracking');
     expect(result.data).toBe(data);
+  });
+
+  it('should log an error when failed to add job to queue', async () => {
+    const data: CreateTrackingDto = {
+      vehicleId: 'test1',
+      timestamp: '2024-08-03T23:58:16.038Z',
+      location: {
+        latitude: 37.793169190714124,
+        longitude: -122.37991487676067,
+        altitude: 28.99365501009916,
+        heading: 218.45610950238648,
+        speed: 60,
+      },
+      vehicleStatus: {
+        engineRunning: false,
+        fuelLevel: 15.35,
+        batteryVoltage: 12.14,
+        odometer: 2050,
+        tirePressure: {
+          frontLeft: 30.19,
+          frontRight: 32.05,
+          rearLeft: 30.83,
+          rearRight: 33.6,
+        },
+      },
+      alerts: [],
+    };
+
+    const spyConsole = jest
+      .spyOn(console, 'error')
+      .mockImplementation(jest.fn());
+
+    mockTrackingQueue.add = jest.fn().mockRejectedValueOnce(new Error());
+    const result = await service.insertWithQueue(data);
+
+    expect(mockTrackingQueue.add).toHaveBeenCalledWith('add-tracking', data);
+    expect(result).toBeUndefined();
+    expect(spyConsole).toHaveBeenCalled();
+  });
+
+  it('should generate report from tracking history', async () => {
+    const expected = [
+      {
+        _id: '66accde6b4c0e66659b93998',
+        hoursOperated: 0,
+        totalDistance: 0,
+        averageSpeed: 60.43,
+        maxSpeed: 60.43,
+        averageTirePressure: {
+          frontLeft: 30.19,
+          frontRight: 32.06,
+          rearLeft: 30.84,
+          rearRight: 33.6,
+        },
+        averageBatteryVoltage: 12.146335705322196,
+        minBatteryVoltage: 12.146335705322196,
+        alertCount: 0,
+      },
+    ];
+    mockTrackingModel.aggregate = jest.fn().mockResolvedValueOnce(expected);
+
+    const result = await service.getVehicleReport('test-1');
+
+    expect(mockTrackingModel.aggregate).toHaveBeenCalledWith(expect.any(Array));
+    expect(result).toEqual(expected);
+  });
+
+  it('should generate report from tracking history', async () => {
+    mockTrackingModel.aggregate = jest.fn().mockRejectedValueOnce(new Error());
+
+    const spyConsole = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+    const result = await service.getVehicleReport('test-1');
+
+    expect(mockTrackingModel.aggregate).toHaveBeenCalledWith(expect.any(Array));
+    expect(result).toBeUndefined();
+    expect(spyConsole).toHaveBeenCalled();
   });
 });
